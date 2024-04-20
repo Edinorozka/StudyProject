91a4c47c69a9d8fa9257866c2b9fdad9ba708a6b
Oleg Erofeev <125142060+Edinorozka@users.noreply.github.com>
2024-04-20 20:20:40 +0300

    Ерофеев Олег Николаевич

---
+++ app/src/main/java/com/example/studyproject/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.studyproject;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    // набор данных, которые свяжем со списком
+    String[] countries = { "Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // получаем элемент ListView
+        ListView countriesList = findViewById(R.id.PostsList);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, countries);
+
+        // устанавливаем для списка адаптер
+        countriesList.setAdapter(adapter);
+    }
+}
